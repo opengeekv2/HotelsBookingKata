31e7a0cec81f446f3a05242d5db2c5bed5232e51
Marc Mauri Alloza <marc.mauri@codurance.com>
2023-05-18 09:23:38 +0200

    chore: work on walking skeleton

--- HotelsBookingKata.Hotels.Domain.Specs/Steps/CalculatorStepDefinitions.cs
+++ HotelsBookingKata.Hotels.Domain.Specs/Steps/CalculatorStepDefinitions.cs
@@ -15,13 +15,7 @@ public sealed class CalculatorStepDefinitions
     [Given("the first number is (.*)")]
     public void GivenTheFirstNumberIs(int number)
     {
-        //TODO: implement arrange (precondition) logic
-        // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-        // To use the multiline text or the table argument of the scenario,
-        // additional string/Table parameters can be defined on the step definition
-        // method. 
-
-        _scenarioContext.Pending();
+        var class1 = Class1.Hallo();
     }
 
     [Given("the second number is (.*)")]
